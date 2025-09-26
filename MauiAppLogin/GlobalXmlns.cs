0d8fcca95860b2bbe5beb1a3426d65f357397fda
yestaque <gabrielbacelar1000@gmail.com>
2025-09-25 21:58:59 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppLogin/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppLogin")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppLogin.Pages")]
